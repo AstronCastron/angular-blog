3739792b508b0d0763a74ac52d167c7c4327088a
AstronCastron <soucastro@outlook.com.br>
2023-10-18 09:57:12 -0300

    Aditional content and routing between IDs and cards

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,60 @@
 export const dataFake = [
 {
    "id":"1",
-   "title": "NOVO HOMEM DE FERRO EM 3D",
-   "description": "Marvel anuncia um novo filme do homem de ferro, confira",
-   "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=506&aspectRatio=2.00&format=jpeg",
+   "title": "3 CURIOSIDADES SOBRE GATOS CALICO",
+   "description": `Você sabia que os gatinhos tricolor são quase sempre fêmeas? Isso acontece por causa de um fenômeno genético chamado calicogênese, que faz com que os genes que determinam a cor do pelo sejam ligados ao cromossomo X. Como as fêmeas têm dois cromossomos X e os machos têm apenas um, é muito mais provável que elas tenham três cores diferentes de pelo.
+
+   Outra curiosidade sobre os gatinhos tricolor é que eles são considerados símbolos de sorte em algumas culturas. No Japão, eles são chamados de maneki-neko, ou gatos que acenam, e são usados como amuletos para atrair prosperidade e felicidade. Na Irlanda, eles são conhecidos como gatos da sorte, e dizem que encontrá-los traz boa sorte.
+   
+   A terceira curiosidade sobre os gatinhos tricolor é que eles têm personalidades únicas e independentes. Eles são geralmente mais inteligentes, curiosos e brincalhões do que outros gatos, e gostam de explorar o ambiente e interagir com as pessoas. Eles também são mais leais e apegados aos seus donos, e podem ser muito protetores e ciumentos.`,
+   "photoCover":"https://www.hepper.com/wp-content/uploads/2022/12/calico-cat-sitting-outdoor_Karen-Kaspar-Shutterstock.jpg",
 },
 {
     "id":"2",
-    "title": "Nova série anunciada no Disney plus",
-    "description": "bla bla bla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
- }
+    "title": "Veja o que este gatinho faz de noite enquanto todos dormem",
+    "description": `
+    Você já se perguntou o que seu gato faz quando você está dormindo? Os gatos são animais noturnos, o que significa que eles estão mais ativos à noite do que durante o dia. Eles podem passar horas brincando, caçando, explorando e fazendo travessuras enquanto você sonha. Aqui estão alguns hábitos noturnos dos gatos que você talvez não saiba:
+
+- Os gatos têm uma visão excelente no escuro. Eles podem ver até seis vezes melhor do que os humanos em condições de pouca luz, graças aos seus olhos grandes e sensíveis. Isso os ajuda a encontrar presas e evitar obstáculos.
+- Os gatos são mestres em se comunicar à noite. Eles podem usar uma variedade de sons, como miados, ronronados, assobios e rosnados, para expressar seus sentimentos, desejos e alertas. Eles também podem usar a linguagem corporal, como orelhas, cauda e bigodes, para transmitir suas intenções.
+- Os gatos adoram dormir em lugares quentes e confortáveis. Eles podem procurar o seu cobertor, travesseiro ou sofá favorito para se aconchegar e relaxar. Eles também podem dormir perto de você, para sentir o seu calor e o seu cheiro. Isso mostra que eles confiam em você e gostam da sua companhia.
+- Os gatos são curiosos por natureza. Eles podem aproveitar a noite para explorar a casa e descobrir novos objetos, cheiros e sons. Eles podem abrir armários, gavetas e portas, mexer em plantas, brinquedos e outros itens, e até mesmo tentar escapar para o exterior. Por isso, é importante manter a casa segura e à prova de gatos, para evitar acidentes ou fugas.
+- Os gatos são brincalhões e divertidos. Eles podem se entreter com qualquer coisa que se mova, faça barulho ou brilhe. Eles podem correr, saltar, perseguir e atacar objetos como bolas, ratinhos, penas e lasers. Eles também podem brincar uns com os outros, se tiverem companheiros felinos. As brincadeiras noturnas ajudam os gatos a gastar energia, exercitar os músculos e estimular o cérebro.
+
+Esses são alguns dos hábitos noturnos dos gatos que tornam esses animais tão fascinantes e adoráveis. Se você tem um gato em casa, tente respeitar o seu ritmo biológico e oferecer-lhe atividades adequadas para a noite. Assim, vocês poderão conviver em harmonia e desfrutar da companhia um do outro.
+    `,
+    "photoCover":"https://i.ytimg.com/vi/nRzsgCp60YU/maxresdefault.jpg"
+ },
+ {
+   "id":"3",
+   "title": "Os gatos ficam doidões! Catnip, afinal é legalizado?",
+   "description": `Você já se perguntou se o catnip é legalizado? Se você é um amante de gatos, provavelmente já ouviu falar dessa erva que deixa os felinos malucos. Mas será que ela faz mal para eles? Será que é proibida em algum lugar? Neste post, vamos esclarecer essas e outras dúvidas sobre o catnip, a maconha dos gatos.
+
+   O que é o catnip?
+   
+   O catnip é uma planta da família da menta, que tem o nome científico de Nepeta cataria. Ela contém uma substância chamada nepetalactona, que ativa os receptores olfativos dos gatos e provoca uma reação comportamental. Os gatos que são sensíveis ao catnip (cerca de 70% deles) costumam rolar, esfregar, lamber e morder a planta, além de ficarem mais relaxados ou agitados, dependendo do indivíduo. O efeito dura cerca de 10 minutos e depois o gato perde o interesse pela erva.
+   
+   O catnip é legalizado?
+   
+   Sim, o catnip é legalizado em todos os países onde é comercializado. Ele não é considerado uma droga, pois não causa dependência nem efeitos colaterais nocivos nos gatos. Na verdade, o catnip pode até trazer benefícios para a saúde e o bem-estar dos bichanos, como aliviar o estresse, estimular o exercício físico e mental, e prevenir problemas dentários. Além disso, o catnip também pode ser usado como repelente natural de insetos, tanto para os gatos quanto para os humanos.
+   
+   Como oferecer o catnip para o seu gato?
+   
+   Existem várias formas de oferecer o catnip para o seu gato, desde a planta fresca ou seca até brinquedos e arranhadores recheados com a erva. O ideal é testar qual a preferência do seu pet e qual a quantidade que ele tolera. Alguns gatos podem ficar muito agressivos ou ansiosos se consumirem muito catnip, por isso é bom moderar a dose e observar a reação. Também é recomendado oferecer o catnip apenas uma vez por semana, para evitar que o gato se acostume e perca o efeito.
+   
+   Agora que você já sabe tudo sobre o catnip, que tal experimentar essa diversão com o seu gato? Ele vai adorar e você vai se divertir muito com as reações dele. Mas lembre-se: o catnip é só para os gatos, ok? Nós humanos não sentimos nada ao cheirar ou comer a planta, então não adianta tentar entrar na onda do seu felino. Aproveite e compartilhe esse post com os seus amigos que também são apaixonados por gatos!(texto gerado pela ia do bing)`,
+   "photoCover":"https://static.boredpanda.com/blog/wp-content/uploads/2018/06/i-take-photos-of-cats-high-on-catnip-fb20.png",
+},
+{
+   "id":"4",
+   "title": "A importância de levar seu bixinho no veterinário. Confira aqui.",
+   "description": `Você ama seus animais de estimação, certo? Então, você sabe que eles precisam de cuidados regulares para manterem-se saudáveis e felizes. Mas você sabia que levar seus animais ao veterinário não é apenas uma questão de prevenção, mas também de diversão?
+
+   Isso mesmo, diversão! Afinal, quem não gosta de ver seu bichinho recebendo carinho, atenção e mimos de um profissional qualificado e simpático? Além disso, você pode aproveitar a visita para tirar suas dúvidas, conhecer outros donos de animais e até fazer novas amizades.
+   
+   Levar seus animais ao veterinário também é uma forma de demonstrar seu amor e responsabilidade por eles. Você estará garantindo que eles estejam protegidos contra doenças, parasitas e outras ameaças à sua saúde. Você também estará contribuindo para o controle populacional de animais, evitando a superlotação de abrigos e o abandono nas ruas.
+   
+   Então, não perca tempo e marque já uma consulta para seu animalzinho. Ele vai agradecer com muitos lambeijos e ronronados. E você vai se sentir mais tranquilo e orgulhoso de ser um dono exemplar.`,
+   "photoCover":"https://pawsandclawsvet.com/wp-content/uploads/2019/06/puppy_kitten.jpg",
+},
 ]
